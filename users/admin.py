1e15069be0d32f5c3edd249102f0c7c6a8b13ec4
DmitriyFomenko1990 <fomenkodmitriy1990@gmail.com>
2026-04-29 21:04:37 +0300

    add user model

---
+++ users/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "username", "is_email_verified", "is_staff", "is_active", "date_joined")
+    list_filter = ("is_email_verified", "is_staff", "is_active")
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Permissions", {"fields": ("is_email_verified", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined", "created_at", "updated_at")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2", "is_email_verified", "is_staff", "is_active"),
+        }),
+    )
+
+    readonly_fields = ("created_at", "updated_at", "last_login", "date_joined")
